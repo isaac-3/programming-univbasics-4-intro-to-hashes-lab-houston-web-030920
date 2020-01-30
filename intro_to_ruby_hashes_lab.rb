3210f606c40e790bce6fea1274f3c813a31926a0
isaac chavez <isaacgc0596@gmail.com>
2020-01-30 01:13:37 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,10 +23,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
-   if hash[key] = 0
-    hash[key] += 1 
+   if update_hash[key] 
+    update_hash[key] += 1 
    else
-    hash[key] = 1
+    update_hash[key] = 1
   end
- return hash
+ return update_hash
 end
